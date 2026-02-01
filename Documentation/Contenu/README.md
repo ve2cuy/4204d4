e4b28037d1f46e547435b932431f3722c1799f7b
ve2cuy <alain.boudreault@me.com>
2026-02-01 08:57:37 -0500

    Ajout à la liste des contenus

--- Documentation/Contenu/README.md
+++ Documentation/Contenu/README.md
@@ -2,8 +2,12 @@
 
 - [Semaine 01 - Présentation du cours et installation des outils](Semaine-01.md)
 - - [Semaine 01 - Labo CLI Docker](labo-semaine01.md) 
-- [Semaine 02 - Introduction à Docker - suite]()
-- [Semaine 03]()
+- [Semaine 02 - Introduction à Docker](https://ve2cuy.github.io/4204d4/Documentation/Docker/Introduction-%C3%A0-Docker.html)
+- - Options -p (ports) et -e (variables d'environnemet)
+- - [Docker Hub, images perso, publication et volumes persistants](https://ve2cuy.github.io/4204d4/Documentation/Docker/Docker-Hub.html)
+- Semaine 3
+- - [YAML](https://ve2cuy.github.io/4204d4/Documentation/Yaml/yaml.html)
+- - [Dockerfile - Introduction](https://ve2cuy.github.io/4204d4/Documentation/Docker/Dockerfile-Introduction.html)
 - [Semaine 04]()
 - [Semaine 05]()
 - [Semaine 06]()
--- Installation/Kubernetes/node.sh
+++ Installation/Kubernetes/node.sh
@@ -10,6 +10,10 @@
 # --------------------------------------------------------------
 # Première partie: Installation et configuration de containerd
 
+# Pré-requis
+sudo apt-get update
+sudo apt-get install -y curl
+
 # 1 - Chargement des modules du noyau requis
 cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf 
 overlay 
@@ -50,16 +54,16 @@ sudo systemctl enable containerd
 # 6 – Désactiver le fichier d’échange de la mémoire virtuelle:
 sudo swapoff -a
 # Pour rendre cette modification permanente, commenter la ligne d’échange dans /etc/fstab
-sudo sed '/\tswap/ s/^\(.*\)$/#\1/g' /etc/fstab
+#sudo sed '/\tswap/ s/^\(.*\)$/#\1/g' /etc/fstab
 
 # 7 – Ajouter le dépôt Kubernetes à la commande ‘apt’:
 # Pré-requis, obtenir la liste de la dernière version stable disponible pour l'installation:
 curl -Ls "https://sbom.k8s.io/$(curl -Ls https://dl.k8s.io/release/stable.txt)/release" | grep "SPDXID: SPDXRef-Package-registry.k8s.io" |  grep -v sha256 | cut -d- -f3- | sed 's/-/\//' | sed 's/-v1/:v1/'
 
 # Utiliser la version entière pour la variable; 1.34.2 = 1.34
-export K8S_VERSION=v1.34
+export K8S_VERSION=v1.35
 sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt-get install -y apt-transport-https ca-certificates gpg
 sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/${K8S_VERSION}/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/${K8S_VERSION}/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
